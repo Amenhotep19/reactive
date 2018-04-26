869a51512c9e6773d7a8267393308220f25f6119
abhi7cr <abhi7cronaldo@gmail.com>
2018-04-26 00:20:02 -0500

    add highlighting to dependents

--- render.js
+++ render.js
@@ -1,4 +1,8 @@
   //only this guy should touch the DOM, kidding not
   export const render = function(template, id) {
     document.getElementById(id).innerHTML = template.apply(this)
+    document.getElementById(id).classList.add('activeProps')
+    setTimeout(() => {
+        document.getElementById(id).classList.remove('activeProps')
+    }, 3000)
   }
